7cd7b4584277b45527b214486279f8120733c85b
Rupam-Mondal <rupammondal564@gmail.com>
2026-07-10 08:42:37 +0530

    Clone and vector mechanism is done

--- Server/src/Services/UrlService.js
+++ Server/src/Services/UrlService.js
@@ -1,7 +1,9 @@
 import {PDFLoader} from '@langchain/community/document_loaders/fs/pdf';
 import {OpenAIEmbeddings} from "@langchain/openai";
 import {QdrantVectorStore} from "@langchain/qdrant";
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
 import dotenv from 'dotenv';
+import fs from "fs/promises";
 import { Clone } from "../Tools/CloneRepo.js";
 import { ReadRepository } from '../Tools/ReadRepo.js';
 
@@ -12,6 +14,42 @@ export async function UrlService(url){
         const clonepath = await Clone(url);
 
         const documents = await ReadRepository(clonepath);
+        console.log(documents.length);
+
+        const splitter = new RecursiveCharacterTextSplitter({
+            chunkSize: 1000,
+            chunkOverlap: 200,
+        });
+
+        const chunks = await splitter.splitDocuments(documents);
+
+        console.log("Chunks :", chunks.length);
+
+        const embeddings = new OpenAIEmbeddings({
+            model: "text-embedding-3-small",
+            apiKey: process.env.OPENAI_API_KEY,
+        });
+
+        const vectorStore = await QdrantVectorStore.fromExistingCollection(
+            embeddings,
+            {
+                url: process.env.QDRUNT_LINK,
+                apiKey: process.env.QDRUNT_KEY,
+                collectionName: "github_project",
+            }
+        );
+
+        await vectorStore.addDocuments(chunks);
+
+        await fs.rm(clonepath, {
+            recursive: true,
+            force: true,
+        });
+
+        return {
+            files: documents.length,
+            chunks: chunks.length,
+        }
         
         
     } catch (error) {
